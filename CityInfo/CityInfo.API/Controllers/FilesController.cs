34ee1d4db412d530b65a1889b665f4a69994a5c9
Luis Gerardo Calderon Contreras <luisg.calderon@devcalcon.com>
2024-04-15 05:45:23 -0600

    Upload file

--- CityInfo/CityInfo.API/Controllers/FilesController.cs
+++ CityInfo/CityInfo.API/Controllers/FilesController.cs
@@ -38,5 +38,28 @@ namespace CityInfo.API.Controllers
             var bytes = System.IO.File.ReadAllBytes(pathFile);
             return File(bytes, contentType, Path.GetFileName(pathFile));
         }
+        [HttpPost]
+        public async Task<ActionResult> CreateFile(IFormFile file)
+        {
+            // Validate the input. Put limit on filesize to avoid large uploads attacks.
+            // Only accept .pdf files (check content-type)
+            if (file.Length == 0 || file.Length > 20971520
+                || file.ContentType != "application/pdf")
+            {
+                return BadRequest("No file or an invalid one has been inputted.");
+            }
+
+            // Creat the file path. Avoid using file. FileName, as an attacker can provide a malicous one, including full paths or relative paths.
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok("Your file has been uploaded succesfully.");
+        }
     }
 }
