61a2323fe765e644108b40a4a7387d35ea90cfb8
Luis Gerardo Calderon Contreras <luisg.calderon@devcalcon.com>
2024-04-16 05:31:40 -0600

    Add Logging with DI

--- CityInfo/CityInfo.API/Program.cs
+++ CityInfo/CityInfo.API/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.StaticFiles;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 // Add services to the container.
 
 builder.Services.AddControllers(options =>
 {
+    
     //options.OutputFormatters
     options.ReturnHttpNotAcceptable = true;
 }).AddNewtonsoftJson()
